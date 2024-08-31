2aff8ce007444f011d8324975046d878f410d381
putri-nayla <putri20820@gmail.com>
2024-08-31 11:51:30 +0700

    membuat perulangan while

---
+++ 02 perulangan while.php
@@ -0,0 +1,13 @@
+<?php 
+
+$ulangi = 0;
+
+while($ulangi < 4){
+    echo"<hr>";
+    echo "<p>Ini adalah perulangan ke-$ulangi</p>";
+    $ulangi++;
+    print_r("<P><strong> by: putri nayla</strong> ");
+    echo"<hr>";
+}
+
+?>
