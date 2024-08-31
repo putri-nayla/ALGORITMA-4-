d886a550dce111732bec039ea098dd276a573a07
putri-nayla <putri20820@gmail.com>
2024-08-31 12:32:15 +0700

    membuat perulangan bersarang

---
+++ 05 perulangan bersarang.php
@@ -0,0 +1,12 @@
+<?php
+$i = 0;
+while($i < 12){
+    for($j = 2; $j < 5; $j++){
+        echo"<hr>";
+        echo "Ini perulangan ke ($i, $j)<br>";
+        echo"<hr>";
+    }
+
+    $i++;
+    print_r("<p><strong> by :putri nayla </strong>");
+}
