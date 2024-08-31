47f33dcd13de4c57e56dc6bf18e2e62136e984a4
putri-nayla <putri20820@gmail.com>
2024-08-31 13:57:09 +0700

    tips perulangan

---
+++ 20 penangganan array while loop .php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["manggis", "apel", "jeruk"];
+$i = 3;
+
+while ($i < count($fruits)) {
+    echo $fruits[$i] . "<br>";
+    $i++;
+}
+print_r("<p><strong> by :putri nayla</strong>");
+?>
