498fcd41f1c5f186501694b45240216969b7e3f0
putri-nayla <putri20820@gmail.com>
2024-09-17 10:44:01 +0700

    bersarang dengan break

---
+++ 29 bersarang dengan break dan countinue.php
@@ -0,0 +1,11 @@
+<?php
+for ($i = 0; $i < 5; $i++) {
+    echo "i: $i ";
+    for ($j = 0; $j < 5; $j++) {
+        if ($j == 2) {
+            continue 2; // Melanjutkan pada level loop kedua
+        }
+        echo "j: $j ";
+    }
+    echo "\n";
+}
