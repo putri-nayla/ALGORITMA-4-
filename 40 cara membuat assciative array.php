1167517707fea4c9332afc80409a3b92a4a5b709
putri-nayla <putri20820@gmail.com>
2024-09-17 12:26:46 +0700

    membuat array

---
+++ 40 cara membuat assciative array.php
@@ -0,0 +1,14 @@
+<?php
+// Menggunakan fungsi array()
+$buah = array(
+    "apel" => "hijau",
+    "strawberry" => "merah",
+    "pisang" => "kuning"
+);
+
+// Menetapkan langsung
+$mobil['Toyota'] = "Corolla";
+$mobil['Honda'] = "Civic";
+$mobil['Suzuki'] = "Swift";
+echo $buah["apel"]; // Output: hijau
+echo $mobil['Honda']; // Output: Civic
