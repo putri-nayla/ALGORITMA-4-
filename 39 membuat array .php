8b7b54998f22782967df1df2ba3da46b7bc4478f
putri-nayla <putri20820@gmail.com>
2024-09-17 12:21:43 +0700

    membuat array

---
+++ 39 membuat array .php
@@ -0,0 +1,14 @@
+<?php
+// Menggunakan fungsi array()
+$buah = array('apel', 'jeruk', 'pisang');
+
+// Menggunakan tanda kurung siku []
+$warna = ['merah', 'hijau', 'biru'];
+// Menggunakan fungsi array()
+$buah = array('apel', 'jeruk', 'pisang');
+
+// Menggunakan tanda kurung siku []
+$warna = ['merah', 'hijau', 'biru'];
+$buah[1] = 'mangga';
+echo $buah[1]; // akan menampilkan 'mangga'
+
